f1c63e980e928d28ab6e388f14f4b15e18cabcc3
Shedrack David <shedrackdavid9@gmail.com>
2022-10-19 06:21:56 -0700

    Empty-Commit

---
+++ 0x03-debugging/2-largest_number.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+
+int largest_number(int a, int b, int c)
+
+{
+	int largest;
+	if (a > b && a > c)
+	{
+		largest = a;
+	}
+	else if (b > c)
+	{
+		largest = b;
+	}
+	else
+	{
+		largest = c;
+	}
+	return (largest);
+
+}
+
+
